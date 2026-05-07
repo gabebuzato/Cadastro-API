85ac1d3a513e92684f5a506e1474647469b11e12
gabebuzato <gabebuzato@gmail.com>
2026-05-07 17:17:02 -0300

    feat: Coloca o lombok na classe para abstração

--- src/main/java/gabebuzato/com/github/CadastroAPI/Users/Controller/Service/UserModel.java
+++ src/main/java/gabebuzato/com/github/CadastroAPI/Users/Controller/Service/UserModel.java
@@ -2,11 +2,15 @@ package gabebuzato.com.github.CadastroAPI.Users.Controller.Service;
 
 import gabebuzato.com.github.CadastroAPI.Tasks.TasksModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 // Entity transforma uma classe numa entidade do DB
 // JPA = Java Persistence API
 
 @Table( name = "tb_cadastro" )
+@NoArgsConstructor // cria os construtores no args
+@AllArgsConstructor // Cria os contrutores all args
+@Data //Cria automaticamente os getters e setters
 @Entity
 public class UserModel {
 
@@ -21,36 +25,4 @@ public class UserModel {
     @ManyToOne
     @JoinColumn(name = "tasks_id") // Foreign Key
     private TasksModel tasks;
-
-    public UserModel() {
-    }
-    public UserModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
