63cd2f21afb1c708842af3a62a123c4938163be1
gabebuzato <gabebuzato@gmail.com>
2026-05-11 12:14:12 -0300

    fix: Arruma alguns erros de versão e coneção com o banco de dados

--- src/main/java/gabebuzato/com/github/CadastroAPI/Tasks/TasksRepository.java
+++ src/main/java/gabebuzato/com/github/CadastroAPI/Tasks/TasksRepository.java
@@ -3,4 +3,5 @@ package gabebuzato.com.github.CadastroAPI.Tasks;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TasksRepository extends JpaRepository<TasksModel, Long> {
+
 }
