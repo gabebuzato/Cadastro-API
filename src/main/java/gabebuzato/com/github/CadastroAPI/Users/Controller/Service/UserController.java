96860fecc37a1e492110bced05a58f6667d8745f
gabebuzato <gabebuzato@gmail.com>
2026-05-12 13:50:26 -0300

    feat: atualiza o controller com mock de dados

--- src/main/java/gabebuzato/com/github/CadastroAPI/Users/Controller/Service/UserController.java
+++ src/main/java/gabebuzato/com/github/CadastroAPI/Users/Controller/Service/UserController.java
@@ -1,8 +1,6 @@
 package gabebuzato.com.github.CadastroAPI.Users.Controller.Service;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,37 @@ public class UserController {
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota";
+    }
+
+    // Adicionar Usuário (CREATE)
+    @PostMapping("/criar")
+    public String criarUsuario() {
+        return "Usuário criado com sucesso";
+    }
+
+
+    // Procurar Usuario por ID (READ)
+    @GetMapping("/todosId")
+    public String mostrarTodosOsUsuariosPorId() {
+        return "Mostrar Usuário por Id";
+    }
+
+    // Mostrar os Usuários (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsUsuarios() {
+        return "Mostrar Usuarios";
+    }
+
+    // Alterar dados do Usuário (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarUsuarioPorID() {
+        return "Informações alteradas com sucesso";
+    }
+
+    // Deletar Usuário (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarUsuarioPorID() {
+        return "Usuario deletado com sucesso";
     }
 
 }
