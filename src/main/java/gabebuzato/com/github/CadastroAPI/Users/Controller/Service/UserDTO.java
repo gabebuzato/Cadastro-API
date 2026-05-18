6621697b60fc10135b91b100cdee656ec6636fcc
gabebuzato <gabebuzato@gmail.com>
2026-05-18 14:19:59 -0300

    feat: adiciona o mapper e o dto a minha aplicação

--- src/main/java/gabebuzato/com/github/CadastroAPI/Users/Controller/Service/UserDTO.java
+++ src/main/java/gabebuzato/com/github/CadastroAPI/Users/Controller/Service/UserDTO.java
@@ -1,4 +1,22 @@
 package gabebuzato.com.github.CadastroAPI.Users.Controller.Service;
 
-public class NinjaDTO {
+import gabebuzato.com.github.CadastroAPI.Tasks.TasksModel;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String rank;
+    private Integer idade;
+    private TasksModel tasks;
 }
--- src/main/java/gabebuzato/com/github/CadastroAPI/Users/Controller/Service/UserMapper.java
+++ src/main/java/gabebuzato/com/github/CadastroAPI/Users/Controller/Service/UserMapper.java
@@ -1,4 +1,29 @@
 package gabebuzato.com.github.CadastroAPI.Users.Controller.Service;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+
+    public UserModel map(UserDTO userDTO) {
+        UserModel userModel = new UserModel();
+        userModel.setId(userDTO.getId());
+        userModel.setNome(userDTO.getNome());
+        userModel.setEmail(userDTO.getEmail());
+        userModel.setIdade(userDTO.getIdade());
+        userModel.setRank(userDTO.getRank());
+        userModel.setTasks(userDTO.getTasks());
+        return userModel;
+    }
+
+    public UserDTO map(UserModel userModel) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(userModel.getId());
+        userDTO.setNome(userModel.getNome());
+        userDTO.setEmail(userModel.getEmail());
+        userDTO.setRank(userModel.getRank());
+        userDTO.setIdade(userModel.getIdade());
+        userDTO.setTasks(userModel.getTasks());
+        return userDTO;
+    }
 }
